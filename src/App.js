970f2caa88718e4fb831a42ad62be8dd28e46f62
Dusan Mijatovic (myLinux 2018) <d.mijatovic@dv4all.nl>
2018-07-15 13:01:21 +0200

    intial commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import logo from './img/logo.svg';
 import './App.scss';
 
+import AddComment from './comment/AddComment';
+import ViewComments from './comment/ViewComments';
+
 class App extends Component {
   render() {
     return (
       <div className="app-body">
         <header className="app-header">
           <img src={logo} className="app-logo" alt="logo" />
-          <h1 className="app-title">Welcome to React SCSS starter</h1>
+          <h1 className="app-title">Welcome to React - Jest demo</h1>
         </header>
-        <p className="app-intro">
-          To get started, read README.md first to learn the possibilities.
-          This project is based on ejected create-react-app setup. SCSS support is added to webpack.  
-        </p>
-        <p className="app-intro">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="app-content">
+          <AddComment className="app-add-comment"/>
+          <ViewComments className="app-view-comments"/>
+        </section>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import AddComment from './comment/AddComment';
+
+
+let div;
+beforeEach(()=>{
+  div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders without crashing', () => { 
+  expect(div).toBeTruthy();
+});
+
+it('has add comment box', () => { 
+  expect(div.innerHTML).toContain(AddComment({className:"app-add-comment"}));
+});
+
+it('has view comments box', () => { 
+  expect(div.innerHTML).toContain("View comments");
+});
+
+afterEach(()=>{
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+
